724532b6c47f0a728255830fcc21693cff30db91
Leocrabe225 <petitscrounchs@yahoo.fr>
2025-05-28 15:32:25 +0200

    Added missing sudo, tee workaroud, temp destination for Makefile, and changed final message

--- cobol_p4_install.sh
+++ cobol_p4_install.sh
@@ -6,18 +6,20 @@ sudo apt-get remove gnucobol3 -y
 
 tar -xf gnucobol.tar.xz
 cd gnucobol-3.1.2
-./configure
+mkdir $ACTUAL_HOME/temp_install
+./configure --prefix=$ACTUAL_HOME/temp_install
 make install
 cd ..
 
+rm -rf $ACTUAL_HOME/temp_install
 rm -rf gnucobol.tar.xz gnucobol-3.1.2
 
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
-echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
 sudo apt update
 sudo apt install postgresql-15 postgresql-client-15 -y
 
-cp pg_hba_trust.conf /etc/postgresql/15/main/pg_hba.conf
+sudo cp pg_hba_trust.conf /etc/postgresql/15/main/pg_hba.conf
 sudo systemctl enable postgresql
 sudo systemctl start postgresql
 sudo systemctl restart postgresql
@@ -25,7 +27,7 @@ sudo systemctl restart postgresql
 psql -U postgres -a -f change_password.sql
 export PGPASSWORD=mdp
 
-cp pg_hba_final.conf /etc/postgresql/15/main/pg_hba.conf
+sudo cp pg_hba_final.conf /etc/postgresql/15/main/pg_hba.conf
 sudo systemctl restart postgresql
 
 psql -U postgres -a -f test.sql
@@ -58,4 +60,5 @@ cd ..
 ocesql testoce.cbl testoce.cob
 cobc -x -locesql -o run testoce.cob
 ./run
-echo "Please not that the postgres db user password is 'mdp', and should be changed"
+echo "Please note that the postgres db user password is 'mdp', and should be changed"
+echo "You must restart the shell (Terminal) before trying to compile with ocesql."
